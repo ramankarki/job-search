304f31046ba3a647c074682d3e890600cb37a3b7
ramankarki <ramankarki40@gmail.com>
2020-12-29 20:21:42 +0545

    declared states for Search component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,32 @@
 import React from "react";
+import Search from "./Search/Search";
+import "./App.css";
 
 export default class App extends React.Component {
+  state = { locationInput: "", searchInput: "" };
+
+  setLocationInput = (locationInput) => {
+    this.setState({ locationInput });
+  };
+
+  setSearchInput = (searchInput) => {
+    this.setState({ searchInput });
+  };
+
+  componentDidUpdate() {
+    console.log(this.state);
+  }
+
   render() {
-    return <div>hello world</div>;
+    return (
+      <React.Fragment>
+        <Search
+          locationInput={this.state.locationInput}
+          setLocationInput={this.setLocationInput}
+          searchInput={this.state.searchInput}
+          setSearchInput={this.setSearchInput}
+        />
+      </React.Fragment>
+    );
   }
 }
