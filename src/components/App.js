9f6dd9ab021e989aad02cca648659ec232b17890
ramankarki <ramankarki40@gmail.com>
2020-12-29 23:26:17 +0545

    provided lastSearchInput,onSearchSubmit,jobs and loading states as props

--- src/components/App.js
+++ src/components/App.js
@@ -3,19 +3,48 @@ import Search from "./Search/Search";
 import "./App.css";
 
 export default class App extends React.Component {
-  state = { locationInput: "", searchInput: "" };
+  state = {
+    searchInput: "",
+    locationInput: "",
+    fullTime: false,
+    jobs: [],
+    lastSearchInput: "",
+    loading: null,
+  };
 
   setLocationInput = (locationInput) => {
     this.setState({ locationInput });
   };
 
   setSearchInput = (searchInput) => {
-    this.setState({ searchInput });
+    this.setState({ searchInput: searchInput });
   };
 
-  componentDidUpdate() {
-    console.log(this.state);
-  }
+  onSearchSubmit = (event) => {
+    event.preventDefault();
+
+    if (!this.state.searchInput) {
+      return;
+    }
+
+    this.setState({ loading: true });
+    this.setState({ lastSearchInput: this.state.searchInput.slice() });
+    this.setLocationInput("");
+    this.setSearchInput("");
+
+    const getJobs = async () => {
+      const data = await fetch(
+        `https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?description=${this.state.searchInput}&location=${this.state.locationInput}`
+      );
+
+      const jsonData = await data.json();
+      this.setState({ jobs: jsonData });
+      this.setState({ loading: false });
+      console.log(this.state);
+    };
+
+    getJobs();
+  };
 
   render() {
     return (
@@ -25,6 +54,10 @@ export default class App extends React.Component {
           setLocationInput={this.setLocationInput}
           searchInput={this.state.searchInput}
           setSearchInput={this.setSearchInput}
+          lastSearchInput={this.state.lastSearchInput}
+          onSearchSubmit={this.onSearchSubmit}
+          jobs={this.state.jobs}
+          loading={this.state.loading}
         />
       </React.Fragment>
     );
