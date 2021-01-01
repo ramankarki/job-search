104f177aeb1a9006bae5345cdf699e4c0352f1d7
ramankarki <ramankarki40@gmail.com>
2021-01-01 11:04:53 +0545

    removed props destructuring

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./css/Details.css";
 import Link from "../Link";
 
-export default function Details({ selectedJob }) {
+export default function Details(props) {
   return (
     <div className="details">
       <aside className="details-links">
@@ -20,7 +20,7 @@ export default function Details({ selectedJob }) {
           <p className="heading">HOW TO APPLY</p>
           <p
             className="content"
-            dangerouslySetInnerHTML={{ __html: selectedJob.how_to_apply }}
+            dangerouslySetInnerHTML={{ __html: props.selectedJob.how_to_apply }}
           ></p>
         </div>
       </aside>
@@ -28,38 +28,38 @@ export default function Details({ selectedJob }) {
         <header className="details-jobTitle">
           <div className="jobTitle-time">
             <h1>
-              <span className="job-title-only">{selectedJob.title}</span>
+              <span className="job-title-only">{props.selectedJob.title}</span>
               <span className="full-time" style={{ marginLeft: "1rem" }}>
                 Full Time
               </span>
             </h1>
             <p className="job-created-time">
               <i className="far fa-clock"></i>
-              {selectedJob.created_at}
+              {props.selectedJob.created_at}
             </p>
           </div>
           <div className="company-logo-location">
             <picture>
               <img
                 src={
-                  selectedJob.company_logo ||
+                  props.selectedJob.company_logo ||
                   "https://placeholder.pics/svg/200/090789/FFFFFF/logo%20not%20found"
                 }
-                alt={selectedJob.company}
+                alt={props.selectedJob.company}
               />
             </picture>
             <div className="company-name-location">
-              <p className="company-name">{selectedJob.company}</p>
+              <p className="company-name">{props.selectedJob.company}</p>
               <p className="company-location">
                 <i className="fas fa-globe-americas"></i>
-                {selectedJob.location}
+                {props.selectedJob.location}
               </p>
             </div>
           </div>
         </header>
         <p
           className="details-description"
-          dangerouslySetInnerHTML={{ __html: selectedJob.description }}
+          dangerouslySetInnerHTML={{ __html: props.selectedJob.description }}
         ></p>
       </article>
     </div>
